ab31186402568858c3c42d28a07e2fdb42f6c602
Oleg Lisov <oleg.lisov163@gmail.com>
2021-12-13 21:57:22 +0400

    code review

--- 1_GOLANG_APP/CRUD.go
+++ 1_GOLANG_APP/CRUD.go
@@ -1,47 +1,55 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 func main() {
-    var employees []string
-    for true {
-        var num int
-        fmt.Println("Выберите пункт для редактирования списка или выхода из меню:\n1.Create\n2.Read\n3.Update\n4.Delete\n5.Exit")
-        fmt.Scan(&num)
-        if num == 1 {
-            var a string
-            fmt.Println("Введите имя работника: ")
-            fmt.Scan(&a)
-            employees = append(employees, a)
-        }
+	var employees []string
+	for true {
+		var num int
+		fmt.Println("Выберите пункт для редактирования списка или выхода из меню:\n1.Create\n2.Read\n3.Update\n4.Delete\n5.Exit")
+		fmt.Scan(&num)
+		if num == 1 {
+			var a string
+			fmt.Println("Введите имя работника: ")
+			fmt.Scan(&a)
+			employees = append(employees, a)
+		}
 
-        if num == 2 {
-            fmt.Println(employees)
-        }
-        if num == 3 {
-            var j int
-            var name string
-            fmt.Println("Введите номер сотрудника, которого необходимо заменить: ")
-            fmt.Scan(&j)
-            fmt.Println("Введите имя нового сотрудника: ")
-            fmt.Scan(&name)
-            employees[j-1] = name
+		if num == 2 {
+			fmt.Println(employees)
+		}
+		if num == 3 {
+			var j int
+			var name string
+			fmt.Println("Введите номер сотрудника, которого необходимо заменить: ")
+			fmt.Scan(&j)
 
-        }
-        if num == 4 {
-            var i int
-            fmt.Println("Введите номер сотрудника, которого необходимо удалить(Отсчет с нуля): ")
-            fmt.Scan(&i)
-            employees = append(employees[:i], employees[i+1:]...)
-        }
-        if num == 5 {
-            os.Exit(3)
-        }
-        if num < 1 || num > 5 {
-            fmt.Println("Ошибка!")
-        }
-    }
+			// здесь желательна проверка на несуществующий номер
+
+			fmt.Println("Введите имя нового сотрудника: ")
+			fmt.Scan(&name)
+			employees[j-1] = name
+
+		}
+		if num == 4 {
+			var i int
+			fmt.Println("Введите номер сотрудника, которого необходимо удалить: ")
+			fmt.Scan(&i)
+
+			// здесь желательна проверка на несуществующий номер
+
+			i-- // чтобы отсчет был также с 1, как и в Update
+			employees = append(employees[:i], employees[i+1:]...)
+		}
+		if num == 5 {
+			// Почему 3? Лучше указывать 0 - нормальное завершение программы
+			os.Exit(3)
+		}
+		if num < 1 || num > 5 {
+			fmt.Println("Ошибка!")
+		}
+	}
 }
--- 2_GOLANG_APP/CRUD.go
+++ 2_GOLANG_APP/CRUD.go
@@ -1,130 +1,145 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "os"
-    "strconv"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"os"
+	"strconv"
 )
 
 type employee struct {
-    id         int
-    last_name  string
-    first_name string
+	id         int
+	last_name  string
+	first_name string
 }
 
 func main() {
 
-    connStr := "user=postgres password=admin dbname=postgres sslmode=disable"
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        db.Exec("CREATE TABLE employee (id SERIAL PRIMARY KEY,first_name varchar(20),last_name varchar(20));")
-    }
-    defer db.Close()
+	connStr := "user=postgres password=admin dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
 
-    for true {
+	// TODO: корректно обработать ошибку (err != nil - была ошибка при выполнении Open)
+	// пример в Slack: канал 2021_ncedu_resources --> go_code_examples_2.zip --> cmd\7_database\database.go
+	if err != nil {
+		db.Exec("CREATE TABLE employee (id SERIAL PRIMARY KEY,first_name varchar(20),last_name varchar(20));")
+		// TODO: обработать ошибку создания таблицы, не завершать программу в любом случае
+	}
+	defer db.Close()
 
-        var num int
-        fmt.Println("Выберите пункт для редактирования списка или выхода из меню:\n1.Create\n2.Read\n3.Update\n4.Delete\n5.Exit")
-        fmt.Scan(&num)
+	for true {
 
-        if num == 1 {
+		var num int
+		fmt.Println("Выберите пункт для редактирования списка или выхода из меню:\n1.Create\n2.Read\n3.Update\n4.Delete\n5.Exit")
+		fmt.Scan(&num)
 
-            var firstN string
-            var lastN string
-            fmt.Println("Введите имя нового сотрудника:")
-            fmt.Scan(&firstN)
-            fmt.Println("Введите фамилию нового сотрудника:")
-            fmt.Scan(&lastN)
+		if num == 1 {
 
-            result, err := db.Exec("INSERT INTO employee (last_name, first_name) VALUES (" + lastN + "," + firstN + ");")
+			var firstN string
+			var lastN string
+			fmt.Println("Введите имя нового сотрудника:")
+			fmt.Scan(&firstN)
+			fmt.Println("Введите фамилию нового сотрудника:")
+			fmt.Scan(&lastN)
 
-            if err != nil {
+			// TODO: использовать плейсхолдеры ($1, $2 и т.д.) для передачи данных в SQL
+			// пример: db.Exec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", "Jason", "Moiron", "jmoiron@jmoiron.net")
+			result, err := db.Exec("INSERT INTO employee (last_name, first_name) VALUES (" + lastN + "," + firstN + ");")
 
-                panic(err)
+			if err != nil {
+				// TODO: обработать ошибку, не используя panic (как минимум вывести на экран)
+				panic(err)
 
-            }
+			}
 
-            fmt.Println(result.RowsAffected())
+			// TODO: вывести что-то более понятное, например, "сотрудник Иванов Иван создан"
+			fmt.Println(result.RowsAffected())
 
-        }
+		}
 
-        if num == 2 {
+		if num == 2 {
 
-            rows, err := db.Query("select * from employee")
-            if err != nil {
-                panic(err)
-            }
-            defer rows.Close()
-            products := []employee{}
+			rows, err := db.Query("select * from employee")
+			if err != nil {
+				// TODO: обработать ошибку, не используя panic (как минимум вывести на экран)
+				panic(err)
+			}
+			defer rows.Close()
+			products := []employee{}
 
-            for rows.Next() {
-                p := employee{}
-                err := rows.Scan(&p.id, &p.last_name, &p.first_name)
-                if err != nil {
-                    fmt.Println(err)
-                    continue
-                }
-                products = append(products, p)
-            }
-            for _, p := range products {
-                fmt.Println(p.id, p.last_name, p.first_name)
-            }
-        }
+			for rows.Next() {
+				p := employee{}
+				err := rows.Scan(&p.id, &p.last_name, &p.first_name)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				products = append(products, p)
+			}
+			for _, p := range products {
+				fmt.Println(p.id, p.last_name, p.first_name)
+			}
+		}
 
-        if num == 3 {
+		if num == 3 {
 
-            var firstN string
-            var lastN string
-            var id int
-            fmt.Println("Введите имя нового сотрудника:")
-            fmt.Scan(&firstN)
-            fmt.Println("Введите фамилию нового сотрудника:")
-            fmt.Scan(&lastN)
-            fmt.Println("Введите номер сотрудника, которого заменить:")
-            fmt.Scan(&id)
+			var firstN string
+			var lastN string
+			var id int
+			fmt.Println("Введите имя нового сотрудника:")
+			fmt.Scan(&firstN)
+			fmt.Println("Введите фамилию нового сотрудника:")
+			fmt.Scan(&lastN)
+			fmt.Println("Введите номер сотрудника, которого заменить:")
+			fmt.Scan(&id)
 
-            result, err := db.Exec("UPDATE employee SET last_name = " + lastN + ",first_name =" + firstN + " WHERE id = " + strconv.Itoa(id) + ";")
+			// TODO: использовать плейсхолдеры ($1, $2 и т.д.) для передачи данных в SQL
+			// нет необходимости использовать strconv.Itoa
+			result, err := db.Exec("UPDATE employee SET last_name = " + lastN + ",first_name =" + firstN + " WHERE id = " + strconv.Itoa(id) + ";")
 
-            if err != nil {
+			if err != nil {
+				// TODO: обработать ошибку, не используя panic (как минимум вывести на экран)
+				panic(err)
 
-                panic(err)
+			}
 
-            }
+			// TODO: вывести что-то более понятное, например, "сотрудник Иванов Иван изменен на Петров Петр"
+			fmt.Println(result.RowsAffected())
 
-            fmt.Println(result.RowsAffected())
+		}
 
-        }
+		if num == 4 {
 
-        if num == 4 {
+			var id int
+			fmt.Println("Введите номер сотрудника, которого удалить:")
+			fmt.Scan(&id)
 
-            var id int
-            fmt.Println("Введите номер сотрудника, которого удалить:")
-            fmt.Scan(&id)
+			// TODO: использовать плейсхолдеры ($1, $2 и т.д.) для передачи данных в SQL
+			// нет необходимости использовать strconv.Itoa
+			result, err := db.Exec("DELETE FROM employee WHERE id=" + strconv.Itoa(id) + ";")
 
-            result, err := db.Exec("DELETE FROM employee WHERE id=" + strconv.Itoa(id) + ";")
+			if err != nil {
+				// TODO: нужно обработать ошибку (как минимум вывести на экран)
+				// не использовать panic, т.к. она аварийно завершает программу
+				panic(err)
 
-            if err != nil {
+			}
 
-                panic(err)
+			// TODO: вывести что-то более понятное, например, "сотрудник Иванов Иван удален"
+			fmt.Println(result.RowsAffected())
 
-            }
+		}
 
-            fmt.Println(result.RowsAffected())
+		if num == 5 {
+			// Почему 3? Лучше указывать 0 - нормальное завершение программы
+			os.Exit(3)
 
-        }
+		}
 
-        if num == 5 {
+		if num < 1 || num > 5 {
 
-            os.Exit(3)
+			fmt.Println("Ошибка!")
 
-        }
-
-        if num < 1 || num > 5 {
-
-            fmt.Println("Ошибка!")
-
-        }
-    }
+		}
+	}
 }
