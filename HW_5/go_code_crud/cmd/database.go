ee0ce38fccef3ef17490ea58ae1f2a5430122fbe
Oleg Lisov <oleg.lisov163@gmail.com>
2021-12-20 17:04:25 +0400

    code review

--- HW_5/go_code_crud/cmd/database.go
+++ HW_5/go_code_crud/cmd/database.go
@@ -9,7 +9,7 @@ import (
 )
 
 type menu struct {
-	id int
+	id   int
 	name string
 }
 
@@ -19,16 +19,24 @@ func main() {
 		log.Fatalln("sqlx.Connect(): ", err)
 	}
 
+	// TODO: Добавить создание таблицы (CREATE TABLE ...) и обработку ошибки создания
+
 	for true {
 
 		var num int
+
+		// TODO: Добавить пункт "Выход"
 		fmt.Println("==========\nВыбери действие и введи соответствующий номер:\n1 - Изучить меню\n2 - Удалить блюдо\n3 - Добавить блюдо\n4 - Редактирование меню\n==========")
 		fmt.Scan(&num)
 		if num == 1 {
 
 			rows, err := db.Query("select * from menu")
 			if err != nil {
+
+				// TODO: Вместо "select * from menu: " вывести сообщение, понятное для обычного пользователя
 				fmt.Println("select * from menu: ", err)
+
+				// TODO: Отсюда вернуться к выбору пунктов
 			}
 			defer rows.Close()
 			products := []menu{}
@@ -52,16 +60,20 @@ func main() {
 			fmt.Println("Введите номер блюда, которое хочешь удалить:")
 			fmt.Scan(&id)
 
+			// TODO: Сделать передачу id в SQL через плейсхолдер $1
 			_, err := db.Query("DELETE FROM menu WHERE id=" + strconv.Itoa(id) + ";")
 
 			if err != nil {
+				// TODO: Вместо "DELETE FROM menu WHERE id" вывести сообщение, понятное для обычного пользователя
 				fmt.Println("DELETE FROM menu WHERE id", err)
 
+				// TODO: Отсюда вернуться к выбору пунктов
 			}
+
+			// TODO: Сделать проверку на удаление данных с помощью RowsAffected() и вывести соответствующие сообщения,
+			// см. пример ниже (там, где UPDATE)
 			fmt.Println("Блюдо было удалено")
 		}
-
-
 
 		if num == 3 {
 
@@ -71,8 +83,11 @@ func main() {
 			_, err := db.Exec("INSERT INTO menu (name) VALUES ($1)", name)
 
 			if err != nil {
+
+				// TODO: вывести корректное сообщение об ошибке, понятное для обычного пользователя, и саму ошибку
 				fmt.Println("Новое блюдо добавлено")
 
+				// TODO: Отсюда вернуться к выбору пунктов
 			}
 			fmt.Println("Добавлено новое блюдо")
 
@@ -87,17 +102,28 @@ func main() {
 			fmt.Println("Введи новое блюдо:")
 			fmt.Scan(&nw)
 
-			_, err := db.Exec("UPDATE menu SET name = ($1) where id = ($2)", nw, id)
+			res, err := db.Exec("UPDATE menu SET name = ($1) where id = ($2)", nw, id)
 
 			if err != nil {
+
+				// TODO: вывести сообщение об ошибке, понятное для обычного пользователя, и саму ошибку
 				fmt.Println("UPDATE menu")
 
+				// TODO: Отсюда вернуться к выбору пунктов
 			}
-			fmt.Println("Блюдо изменено")
+
+			// пример проверки изменения данных
+			if rowCount, _ := res.RowsAffected(); rowCount > 0 {
+				fmt.Printf("Блюдо с номером %d изменено на %s\n", id, nw)
+			} else {
+				fmt.Printf("Ошибка: Блюдо с номером %d не изменено\n", id)
+			}
 
 		}
 		if num < 1 || num > 4 {
 			fmt.Println("Ошибка!")
 		}
+
+		// TODO: Добавить закрытие базы данных
 	}
 }
