c34b98bacf17581e2b17c1cffb85e53c9b7256fd
Sergey <sergokulik61@gmail.com>
2021-12-22 18:04:21 +0400

    Database update

--- HW_5/go_code_crud/cmd/database.go
+++ HW_5/go_code_crud/cmd/database.go
@@ -18,7 +18,10 @@ func main() {
 	if err != nil {
 		log.Fatalln("sqlx.Connect(): ", err)
 	}
-	_, err = db.Query("create table menu(id serial,name varchar)")
+	_, err = db.Exec("create table menu(id serial,name varchar)")
+	if err != nil {
+		fmt.Println("Меню не создано ", err)
+	}
 
 	for true {
 
@@ -32,8 +35,7 @@ func main() {
 			if err != nil {
 
 				fmt.Println("Ошибка при изучении меню ", err)
-
-				break;
+				continue
 			}
 			defer rows.Close()
 			products := []menu{}
@@ -57,64 +59,65 @@ func main() {
 			fmt.Println("Введите номер блюда, которое хочешь удалить:")
 			fmt.Scan(&id)
 
-			_, err := db.Query("DELETE FROM menu WHERE id=($1);",id)
+			rw, err := db.Exec("DELETE FROM menu WHERE id=($1);", id)
 
 			if err != nil {
 				fmt.Println("Блюдо не удалено ", err)
-
-				break;
+				continue
 			}
-
-			fmt.Println("Блюдо было удалено")
-		}
-
-		if num == 3 {
-
-			var name string
-			fmt.Println("Введи название нового блюда (оно добавиться в конец меню):")
-			fmt.Scan(&name)
-			_, err := db.Exec("INSERT INTO menu (name) VALUES ($1)", name)
-
-			if err != nil {
-
-				fmt.Println("Новое блюдо не добавлено ", err)
-
-				break;
-			}
-			fmt.Println("Добавлено новое блюдо")
-
-		}
-
-		if num == 4 {
-
-			var id int
-			var nw string
-			fmt.Println("Введи номер блюда для изменения:")
-			fmt.Scan(&id)
-			fmt.Println("Введи новое блюдо:")
-			fmt.Scan(&nw)
-
-			res, err := db.Exec("UPDATE menu SET name = ($1) where id = ($2)", nw, id)
-
-			if err != nil {
-
-				fmt.Println("Меню не изменено", err)
-
-			}
-
-			if rowCount, _ := res.RowsAffected(); rowCount > 0 {
-				fmt.Printf("Блюдо с номером %d изменено на %s\n", id, nw)
+			if rowCount, _ := rw.RowsAffected(); rowCount > 0 {
+				fmt.Println("Блюдо было удалено")
 			} else {
-				fmt.Printf("Ошибка: Блюдо с номером %d не изменено\n", id)
+				fmt.Println("Блюдо не было удалено")
+			}
+			if num == 3 {
+
+				var name string
+				fmt.Println("Введи название нового блюда (оно добавиться в конец меню):")
+				fmt.Scan(&name)
+				_, err := db.Exec("INSERT INTO menu (name) VALUES ($1)", name)
+
+				if err != nil {
+
+					fmt.Println("Новое блюдо не добавлено ", err)
+
+					continue
+				}
+				fmt.Println("Добавлено новое блюдо")
+
 			}
 
+			if num == 4 {
+
+				var id int
+				var nw string
+				fmt.Println("Введи номер блюда для изменения:")
+				fmt.Scan(&id)
+				fmt.Println("Введи новое блюдо:")
+				fmt.Scan(&nw)
+
+				res, err := db.Exec("UPDATE menu SET name = ($1) where id = ($2)", nw, id)
+
+				if err != nil {
+
+					fmt.Println("Меню не изменено", err)
+					continue
+				}
+
+				if rowCount, _ := res.RowsAffected(); rowCount > 0 {
+					fmt.Printf("Блюдо с номером %d изменено на %s\n", id, nw)
+				} else {
+					fmt.Printf("Ошибка: Блюдо с номером %d не изменено\n", id)
+				}
+
+			}
+			if num == 5 {
+				os.Exit(0)
+			}
+			if num < 1 || num > 5 {
+				fmt.Println("Ошибка!")
+			}
 		}
-		if num == 5 {
-			os.Exit(0)
-		}
-		if num < 1 || num > 5 {
-			fmt.Println("Ошибка!")
-		}
+		db.Close()
 	}
-	db.Close();
 }
