e372d9054fba48078567b7fb15146f9cd659c86a
Segey <71715087+art-zombie@users.noreply.github.com>
2022-03-18 14:14:57 +0400

    Delete Отдел кадрров directory

--- Отдел кадрров/src/main/java/HRdepartment/demo/repository/PositionRepository.java
+++
@@ -1,9 +0,0 @@
-package HRdepartment.demo.repository;
-
-import HRdepartment.demo.model.Position;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PositionRepository extends CrudRepository<Position, Integer> {
-}
--- Отдел кадрров/src/main/java/HRdepartment/demo/repository/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package HRdepartment.demo.repository;
-
-import HRdepartment.demo.model.Worker;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorkerRepository extends CrudRepository<Worker, Integer> {
-}
